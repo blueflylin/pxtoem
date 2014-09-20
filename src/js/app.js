cc138a38b6142d16757aa482aa7ce39d1ed4b6bc
Martin Schenck <herrschenck@gmail.de>
2014-09-20 18:42:45 +0200

    first working version

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,27 @@
 var pxtoemApp = angular.module('pxtoemApp', []);
 
-pxtoemApp.controller('pxToEmCtl', function($scope) {
-    $scope.px = 'px';
+pxtoemApp.controller('pxToEmCtl', function($scope, $filter) {
+    $scope.px = '';
+    $scope.em = '';
+    $scope.base = 16;
+
+    $scope.pxChange = function() {
+        $scope.em = $scope.px / $scope.base;
+        $scope.em = $filter('number')($scope.em, 4);
+    };
+    $scope.emChange = function() {
+        $scope.px = $scope.em * $scope.base;
+        $scope.px = $filter('number')($scope.px, 0);
+    };
+    $scope.baseChange = function() {
+        $scope.em = $scope.px / $scope.base;
+        $scope.em = $filter('number')($scope.em, 4);
+    };
+
+    $scope.pixels = [];
+    for (var i = 6; i <= 25; i++) {
+        $scope.pixels.push(i);
+    }
+    $scope.pixels.push(30);
+    $scope.pixels.push(45);
 });
