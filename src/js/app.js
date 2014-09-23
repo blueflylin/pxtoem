f8aaf3b6b1ff11cc2df813bc441c9085a2cedd72
Martin Schenck <martin.schenck@plugsurfing.com>
2014-09-23 19:15:15 +0200

    Fixes #3 (periods and commas and so on)

--- src/js/app.js
+++ src/js/app.js
@@ -5,16 +5,44 @@ pxtoemApp.controller('pxToEmCtl', function($scope, $filter) {
     $scope.em = '';
     $scope.base = 16;
 
+    /**
+     * Replaces commas with periods or removes both if not desired
+     */
+    function fixCommas() {
+        if (typeof $scope.px === 'string') {
+            $scope.px = $scope.px.replace(',', '');
+            if (typeof $scope.px === 'string') {
+                $scope.px = $scope.px.replace('.', '');
+            }
+        }
+        if (typeof $scope.em === 'string') {
+            $scope.em = $scope.em.replace(',', '.');
+
+        }
+        if (typeof $scope.base === 'string') {
+            $scope.base = $scope.base.replace(',', '');
+            if (typeof $scope.base === 'string') {
+                $scope.base = $scope.base.replace('.', '');
+            }
+        }
+    }
+
     $scope.pxChange = function() {
+        fixCommas();
+
         $scope.em = $scope.px / $scope.base;
         $scope.em = $filter('number')($scope.em, 3);
     };
     $scope.emChange = function() {
+        fixCommas();
+
         $scope.px = $scope.em * $scope.base;
         $scope.px = $filter('number')($scope.px, 0);
     };
     $scope.baseChange = function() {
-        newEm = $scope.px / $scope.base;
+        fixCommas();
+
+        var newEm = $scope.px / $scope.base;
 
         if (newEm > 0) {
             newEm = $filter('number')(newEm, 3);
